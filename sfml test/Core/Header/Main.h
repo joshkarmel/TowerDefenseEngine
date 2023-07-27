c2295495669c018b9a50a576e6593c4066fdd93e
katarinamechanics <mattbrodowski1@yahoo.com>
2023-07-26 21:43:03 -0400

    init

---
+++ sfml test/Core/Header/Main.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "SFML/Graphics.hpp"
+#include "SFML/Main.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
+
+#include <iostream>
+
+const int MmapHeight = 1024;
+int MmapWidth = 1920;
+
+sf::Clock DeltaClock;
+
+
---
+++ sfml test/Core/src/Main.cpp
@@ -0,0 +1,33 @@
+#include "../Header/Main.h"
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(MmapWidth, MmapHeight), "Testing Box");
+   
+
+    sf::Vector2i mousePosition;
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+           // ImGui::SFML::ProcessEvent(window, event);
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+        mousePosition = sf::Mouse::getPosition(window);
+
+        
+
+        DeltaClock.restart();
+        
+        
+       // ImGui::SFML::Render(window); //abstract later
+        window.display();
+    }
+  
+
+    return 0;
+}
